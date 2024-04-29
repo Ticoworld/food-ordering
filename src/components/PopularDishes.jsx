70f36f0b0f2caf58130294439746079190a6bcbb
Ticoworld <timothyticoworld@gmail.com>
2024-04-29 16:55:43 +0100

    n

--- src/components/PopularDishes.jsx
+++ src/components/PopularDishes.jsx
@@ -102,8 +102,8 @@ const PopularDishes = () => {
                         <img src={popularDish.image} alt=""  className="lg:w-44 w-24 rounded-md lg:h-36 h-[5rem] object-cover "/>   
                         </div>
                         <div className="details w-full">
-                            <h5 className="text-yellow-700 font-semibold lg:text-lg text-xs uppercase text-wrap ">{popularDish.dishName}</h5>
-                            <div className="flex items-center justify-between lg:gap-2 gap-1 py-2">
+                            <h5 className="text-yellow-700 font-semibold lg:text-lg text-xs uppercase text-wrap pt-1 ">{popularDish.dishName}</h5>
+                            <div className="flex items-center justify-between lg:gap-2 gap- py-1">
                             <button title="buy now"
                             className="bg-red-500 text-white lg:py-2 lg:px-4 px-1 py-1 text-xs lg:text-lg rounded-md cursor-pointer w-full"
                         >
